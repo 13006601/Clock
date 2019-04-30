1671244c768662365c546fb11dd6aa5f27956d80
Maksims Mizgins 13006601 <>
2019-04-30 09:38:19 +0100

    new commit

--- src/clock/View.java
+++ src/clock/View.java
@@ -1,21 +1,32 @@
 package clock;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.Date;
 import javax.swing.*;
 import java.util.Observer;
 import java.util.Observable;
+import javax.swing.SpinnerDateModel;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.JPanel;
 
-public class View implements Observer {
+public class View implements Observer{
     
     ClockPanel panel;
+    JButton AddAlarm;
+
     
     public View(Model model) {
         JFrame frame = new JFrame();
         panel = new ClockPanel(model);
         //frame.setContentPane(panel);
         frame.setTitle("Java Clock");
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+     
         // Start of border layout code
         
         // I've just put a single button in each of the border positions:
@@ -28,28 +39,61 @@ public class View implements Observer {
         
         Container pane = frame.getContentPane();
         
-        JButton button = new JButton("Button 1 (PAGE_START)");
-        pane.add(button, BorderLayout.PAGE_START);
-         
-        panel.setPreferredSize(new Dimension(200, 200));
-        pane.add(panel, BorderLayout.CENTER);
-         
-        button = new JButton("Button 3 (LINE_START)");
+        JButton button = new JButton("Add");
         pane.add(button, BorderLayout.LINE_START);
          
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
-        pane.add(button, BorderLayout.PAGE_END);
+        panel.setPreferredSize(new Dimension(200, 200));
+        pane.add(panel, BorderLayout.PAGE_START);
          
-        button = new JButton("5 (LINE_END)");
-        pane.add(button, BorderLayout.LINE_END);
+       JButton button2 = new JButton("Update");
+        pane.add(button2, BorderLayout.CENTER);
+         
+        JButton button3 = new JButton("Delete");
+        pane.add(button3, BorderLayout.LINE_END);
+         
+        JButton button4 = new JButton("Save");
+        pane.add(button4, BorderLayout.PAGE_END);
         
         // End of borderlayout code
         
         frame.pack();
         frame.setVisible(true);
+        
+        button.addActionListener(new ActionListener() {
+            
+          
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+               
+                AddAlarm();
+                
+               
+            }
+    
+        });
+        
+        
     }
     
+    @Override
     public void update(Observable o, Object arg) {
         panel.repaint();
     }
-}
+    
+    public void AddAlarm() {
+        Date date = new Date();
+        SpinnerDateModel sm =
+        new SpinnerDateModel(date,null,null,Calendar.HOUR_OF_DAY);
+        JSpinner spinner = new JSpinner(sm);
+        int option = JOptionPane.showOptionDialog(null, spinner, "Add alarm time", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+        if (option == JOptionPane.CANCEL_OPTION){
+            // user hit cancel
+        } else if (option == JOptionPane.OK_OPTION){
+            // user entered a number
+           String message = JOptionPane.showInputDialog("Alarm message",JOptionPane.INFORMATION_MESSAGE);
+        }
+        //JSpinner.DateEditor de = new JSpinner.DateEditor(spinner,"HH:mm:ss");
+        //spinner.setEditor(de);
+        
+    }
+}  
